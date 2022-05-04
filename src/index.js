8c6ded6e1ce036771d244d114bdd50d8f28aed2f
Tal Giladi <talgiladi@gmail.com>
2022-05-04 08:01:32 -0700

    2

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import _ from 'lodash';
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
@@ -15,7 +16,11 @@ class App extends Component{
       videos:[],
       selectedVideo:null
     };
-    YTSearch({key:API_KEY, term:'surfboards'},(videos)=>{
+    this.videoSearch('surfboards');
+  }
+
+  videoSearch(term){
+    YTSearch({key:API_KEY, term:term},(videos)=>{
       console.log('loaded videos');
       this.setState({
         videos:videos,
@@ -25,9 +30,10 @@ class App extends Component{
   }
 
   render(){
+    const videoSearch = _.debounce((term) =>{ this.videoSearch(term)},300);
     return (
       <div>
-      <SearchBar />
+      <SearchBar onSearchTermChange={videoSearch} />
       <VideoDetail video={this.state.selectedVideo}/>
       <VideoList
         onVideoSelect={selectedVideo => this.setState({selectedVideo})}
