bc678521bff3dc16f549f194dec2de518775a58c
Tal Giladi <talgiladi@gmail.com>
2022-05-04 00:21:11 -0700

    first commit

--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
 
-import App from './components/app';
-import reducers from './reducers';
+import SearchBar from './components/search_bar';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const API_KEY = 'AIzaSyCvAfhVnzexMSpDmLfHxzFjYo7rXy5vqAg';
+const App = () => {
+  return (
+    <div>
+    <SearchBar />
+  </div>
+  );
+};
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+
+ReactDOM.render(<App />, document.querySelector(('.container')));
