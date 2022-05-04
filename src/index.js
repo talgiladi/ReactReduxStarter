ce30855813bd8c03445766a08bb5a912a477cec0
Tal Giladi <talgiladi@gmail.com>
2022-05-04 05:57:21 -0700

    2

--- src/index.js
+++ src/index.js
@@ -1,16 +1,41 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
-
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
-
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 const API_KEY = 'AIzaSyCvAfhVnzexMSpDmLfHxzFjYo7rXy5vqAg';
-const App = () => {
-  return (
-    <div>
-    <SearchBar />
-  </div>
-  );
-};
+
+
+
+class App extends Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      videos:[],
+      selectedVideo:null
+    };
+    YTSearch({key:API_KEY, term:'surfboards'},(videos)=>{
+      console.log('loaded videos');
+      this.setState({
+        videos:videos,
+        selectedVideo:videos[0]
+      })
+    });
+  }
+
+  render(){
+    return (
+      <div>
+      <SearchBar />
+      <VideoDetail video={this.state.selectedVideo}/>
+      <VideoList
+        onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+        videos={this.state.videos} />
+    </div>
+    );
+  }
+}
 
 
 ReactDOM.render(<App />, document.querySelector(('.container')));
